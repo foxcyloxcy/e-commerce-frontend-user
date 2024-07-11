c66e2706fb7acae2fcaab31403664d2f0a0c3159
3Lsome <luckyllemos0909@gmail.com>
2024-07-12 07:16:10 +0800

    Update add theme component

---
+++ src/components/ThemeComponent/ModTheme.jsx
@@ -0,0 +1,83 @@
+// theme.js
+import { createTheme } from '@mui/material/styles';
+
+const ModTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#255773',
+            light: '#5E97C3',
+            dark: '#B8D4E6',
+            contrastText: '#E3F2F7',
+        },
+        secondary: {
+            main: '#606060',
+            light: '#BDB6B0',
+            dark: '#EAEAE7',
+            contrastText: '#F5F5F2',
+            background: '#FFFFFF',
+        },
+    },
+    typography: {
+        fontFamily: 'Poppins, sans-serif',
+        h1: {
+            fontFamily: 'Playfair Display, serif',
+            fontSize: '2rem',
+            '@media (min-width:400px)': {
+                fontSize: '2.5rem',
+            },
+            '@media (min-width:600px)': {
+                fontSize: '3rem',
+            },
+            '@media (min-width:960px)': {
+                fontSize: '3.5rem',
+            },
+            '@media (min-width:1280px)': {
+                fontSize: '4rem',
+            },
+        },
+        body1: {
+            fontSize: '0.75rem',
+            '@media (min-width:400px)': {
+                fontSize: '1rem',
+            },
+            '@media (min-width:600px)': {
+                fontSize: '1.125rem',
+            },
+            '@media (min-width:960px)': {
+                fontSize: '1.25rem',
+            },
+            '@media (min-width:1280px)': {
+                fontSize: '1.375rem',
+            },
+        },
+        body2: {
+            fontSize: '0.75rem',
+            '@media (min-width:400px)': {
+                fontSize: '0.875rem',
+            },
+            '@media (min-width:600px)': {
+                fontSize: '1rem',
+            },
+            '@media (min-width:960px)': {
+                fontSize: '1.125rem',
+            },
+            '@media (min-width:1280px)': {
+                fontSize: '1.25rem',
+            },
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 0,
+                    '&:hover': {
+                        color: '#F5F5F2', // secondary contrast text color
+                    },
+                },
+            },
+        },
+    },
+});
+
+export default ModTheme;
