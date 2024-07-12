460d8019efa2920bb75d2724e70fae0fc235541b
3Lsome <luckyllemos0909@gmail.com>
2024-07-12 10:00:11 +0800

    Update add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import React, { useState } from 'react';
 import './App.css'
 import Login from './components/LoginComponent/Login'
 import Register from './components/RegisterComponent/Register'
+import NavBar from './components/NavbarComponent/NavBar'
 
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
-    <Login/>
-    <Register/>
+      <NavBar isLoggedIn={isLoggedIn} />
+      <Login />
+      <Register />
     </>
   )
 }
