f3eea1b9f195d26037f6a28d5181358e0df63bb6
3Lsome <luckyllemos0909@gmail.com>
2024-07-12 10:00:49 +0800

    Update navbar styling

--- src/components/NavbarComponent/NavBar.jsx
+++ src/components/NavbarComponent/NavBar.jsx
@@ -4,31 +4,19 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Slide from '@mui/material/Slide';
+import CloseIcon from '@mui/icons-material/Close';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, Box, useMediaQuery, Button, Drawer, List, ListItem, ListItemText, Divider, Backdrop, ListItemIcon } from '@mui/material';
+import ModTheme from '../ThemeComponent/ModTheme';
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
-
-const HideOnScroll = (props) => {
-  const { children } = props;
-  const trigger = useScrollTrigger();
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-};
 
 const NavBar = ({ isLoggedIn }) => {
   const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleMenu = () => {
+    setAnchorEl(true);
   };
 
   const handleClose = () => {
@@ -36,19 +24,29 @@ const NavBar = ({ isLoggedIn }) => {
   };
 
   const [elevate, setElevate] = useState(false);
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
 
-  useEffect(() => {
-    setElevate(trigger);
-  }, [trigger]);
+  if (isLoggedIn) {
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+
+    useEffect(() => {
+      setElevate(trigger);
+    }, [trigger]);
+  }
+
+  const isSmallScreen = useMediaQuery(ModTheme.breakpoints.down('sm'));
+  const isMediumScreen = useMediaQuery(ModTheme.breakpoints.down('md')) && !isSmallScreen;
+
+  console.log(ListItemText)
+  console.log(<Button color="inherit">Home</Button>
+  )
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <HideOnScroll>
+    <>
+      <ThemeProvider theme={ModTheme}>
+        <CssBaseline />
         <AppBar
           position={isLoggedIn ? 'sticky' : 'static'}
           color="transparent"
@@ -63,7 +61,7 @@ const NavBar = ({ isLoggedIn }) => {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               My Website
             </Typography>
-            <div>
+            {isSmallScreen || isMediumScreen ? (
               <IconButton
                 edge="start"
                 color="inherit"
@@ -72,33 +70,95 @@ const NavBar = ({ isLoggedIn }) => {
               >
                 <MenuIcon />
               </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+            ) : (
+              <Box sx={{ display: 'flex', gap: 2 }}>
+                <Button color="inherit">Home</Button>
+                <Button color="inherit">Shop</Button>
+                <Button color="inherit">About</Button>
+                <Button color="inherit">Contact Us</Button>
+              </Box>
+            )}
+            <Drawer
+              anchor="right"
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              sx={{
+                '& .MuiDrawer-paper': {
+                  width: '50%',
+                },
+              }}
+            >
+              <Box
+                sx={{
+                  width: '100%',
+                  height: '20%',
+                  backgroundColor: ModTheme.palette.background.default,
+                  display: 'flex',
+                  flexDirection: 'row', // Changed to row alignment
+                  justifyContent: 'space-around', // Adjusted for spacing
+                  alignItems: 'center',
+                  padding: '10px', // Added padding for spacing
                 }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
               >
-                <MenuItem onClick={handleClose}>Home</MenuItem>
-                <MenuItem onClick={handleClose}>Shop</MenuItem>
-                <MenuItem onClick={handleClose}>About</MenuItem>
-                <MenuItem onClick={handleClose}>Contact Us</MenuItem>
-              </Menu>
-            </div>
+                <List component="nav" sx={{ display: 'flex', flexDirection: 'row' }}>
+                  <ListItem button sx={{ flexGrow: 1 }}>
+                    <ListItemText primary="Login" />
+                  </ListItem>
+                  <Divider orientation="vertical" flexItem />
+                  <ListItem button sx={{ flexGrow: 1 }}>
+                    <ListItemText primary="Register" />
+                  </ListItem>
+                </List>
+                <IconButton color="inherit" onClick={handleClose}>
+                  <CloseIcon />
+                </IconButton>
+              </Box>
+              <Box
+                sx={{
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: ModTheme.palette.background.default,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <List component="nav">
+                  <ListItem button>
+                    <ListItemText primary="Home" />
+                  </ListItem>
+                  <Divider />
+                  <ListItem button>
+                    <ListItemText primary="Shop" />
+                  </ListItem>
+                  <Divider />
+                  <ListItem button>
+                    <ListItemText primary="About" />
+                  </ListItem>
+                  <Divider />
+                  <ListItem button>
+                    <ListItemText primary="Contact Us" />
+                  </ListItem>
+                </List>
+              </Box>
+            </Drawer>
+            {isMediumScreen && (
+              <Backdrop
+                open={Boolean(anchorEl)}
+                onClick={handleClose}
+                sx={{
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                  zIndex: (theme) => theme.zIndex.drawer - 1,
+                }}
+              />
+            )}
           </Toolbar>
         </AppBar>
-      </HideOnScroll>
-      <Offset />
+        <Offset />
+      </ThemeProvider>
       {/* Content goes here */}
-    </React.Fragment>
+    </>
   );
 };
 
