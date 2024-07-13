011a1732064f1ade6dfe8fee7770b32aac4a2318
3Lsome <luckyllemos0909@gmail.com>
2024-07-13 08:28:08 +0800

    Update add new components to be render

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import './App.css'
 import Login from './components/LoginComponent/Login'
 import Register from './components/RegisterComponent/Register'
 import NavBar from './components/NavbarComponent/NavBar'
+import Footer from './components/FooterComponent/Footer';
+import FeaturedHero from './components/HeroComponent/FeaturedHero'
 
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
     <>
       <NavBar isLoggedIn={isLoggedIn} />
+      <FeaturedHero/>
       <Login />
       <Register />
+      <Footer/>
     </>
   )
 }
