8f12c495fae145a0b1d36795c9db44340b14d93a
3Lsome <luckyllemos0909@gmail.com>
2024-07-13 08:31:36 +0800

    Update modify styling for buttons

--- src/components/NavbarComponent/NavBar.jsx
+++ src/components/NavbarComponent/NavBar.jsx
@@ -7,13 +7,16 @@ import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider, Box, useMediaQuery, Button, Drawer, List, ListItem, ListItemText, Divider, Backdrop, ListItemIcon } from '@mui/material';
+import { ThemeProvider, Box, useMediaQuery, Button, Drawer, List, ListItem, ListItemText, Divider, Backdrop } from '@mui/material';
 import ModTheme from '../ThemeComponent/ModTheme';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import ButtonComponent from '../ButtonComponent/ButtonComponent';
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 const NavBar = ({ isLoggedIn }) => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [elevate, setElevate] = useState(false);
 
   const handleMenu = () => {
     setAnchorEl(true);
@@ -22,8 +25,6 @@ const NavBar = ({ isLoggedIn }) => {
   const handleClose = () => {
     setAnchorEl(null);
   };
-
-  const [elevate, setElevate] = useState(false);
 
   if (isLoggedIn) {
     const trigger = useScrollTrigger({
@@ -39,27 +40,24 @@ const NavBar = ({ isLoggedIn }) => {
   const isSmallScreen = useMediaQuery(ModTheme.breakpoints.down('sm'));
   const isMediumScreen = useMediaQuery(ModTheme.breakpoints.down('md')) && !isSmallScreen;
 
-  console.log(ListItemText)
-  console.log(<Button color="inherit">Home</Button>
-  )
-
   return (
     <>
       <ThemeProvider theme={ModTheme}>
         <CssBaseline />
         <AppBar
-          position={isLoggedIn ? 'sticky' : 'static'}
+          position={elevate ? 'sticky' : 'absolute'}
           color="transparent"
           elevation={elevate ? 4 : 0}
           sx={{
-            backgroundColor: elevate ? 'white' : 'transparent',
-            transition: 'background-color 0.5s, box-shadow 0.5s',
-            boxShadow: elevate ? '0px 4px 20px rgba(0, 0, 0, 0.1)' : 'none',
+            transform: 'translate(0, 0)',
+            backgroundColor: elevate ? ModTheme.palette.primary.dark : 'transparent',
+            transition: 'background-color 0.75s, box-shadow 0.75s',
+            boxShadow: elevate ? '0px 4px 20px rgba(0, 0, 0, 0.3)' : 'none',
           }}
         >
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              My Website
+              Reloved Logo
             </Typography>
             {isSmallScreen || isMediumScreen ? (
               <IconButton
@@ -72,10 +70,33 @@ const NavBar = ({ isLoggedIn }) => {
               </IconButton>
             ) : (
               <Box sx={{ display: 'flex', gap: 2 }}>
-                <Button color="inherit">Home</Button>
-                <Button color="inherit">Shop</Button>
-                <Button color="inherit">About</Button>
-                <Button color="inherit">Contact Us</Button>
+                <ButtonComponent
+                  label="Home"
+                  textColor={elevate ? 'secondary.main' : 'primary.contrastText'}
+                  hoverTextColor={elevate ? 'primary.main' : 'primary.light'}
+                />
+                <ButtonComponent
+                  label="Shop"
+                  textColor={elevate ? 'secondary.main' : 'primary.contrastText'}
+                  hoverTextColor={elevate ? 'primary.main' : 'primary.light'}
+                />
+                <ButtonComponent
+                  label="About"
+                  textColor={elevate ? 'secondary.main' : 'primary.contrastText'}
+                  hoverTextColor={elevate ? 'primary.main' : 'primary.light'}
+                />
+                <ButtonComponent
+                  label="Login"
+                  buttonVariant="contained"
+                  textColor='primary.contrastText'
+                  hoverTextColor='secondary.main'
+                />
+                <ButtonComponent
+                  label="Register"
+                  buttonVariant="contained"
+                  textColor='primary.contrastText'
+                  hoverTextColor='secondary.main'
+                />
               </Box>
             )}
             <Drawer
@@ -94,24 +115,30 @@ const NavBar = ({ isLoggedIn }) => {
                   height: '20%',
                   backgroundColor: ModTheme.palette.background.default,
                   display: 'flex',
-                  flexDirection: 'row', // Changed to row alignment
-                  justifyContent: 'space-around', // Adjusted for spacing
+                  flexDirection: 'column', // Changed to column alignment
+                  justifyContent: 'space-between', // Adjusted for spacing
                   alignItems: 'center',
-                  padding: '10px', // Added padding for spacing
+                  padding: '10px',
                 }}
               >
-                <List component="nav" sx={{ display: 'flex', flexDirection: 'row' }}>
-                  <ListItem button sx={{ flexGrow: 1 }}>
-                    <ListItemText primary="Login" />
-                  </ListItem>
-                  <Divider orientation="vertical" flexItem />
-                  <ListItem button sx={{ flexGrow: 1 }}>
-                    <ListItemText primary="Register" />
-                  </ListItem>
-                </List>
-                <IconButton color="inherit" onClick={handleClose}>
+                <IconButton color="inherit" onClick={handleClose} sx={{ alignSelf: 'flex-end' }}>
                   <CloseIcon />
                 </IconButton>
+                <List component="nav" sx={{ display: 'flex', flexDirection: 'row', gap: 2, }}>
+                  <ButtonComponent
+                    label="Login"
+                    buttonVariant="contained"
+                    textColor='primary.contrastText'
+                    hoverTextColor='secondary.main'
+                  />
+                  <Divider orientation="vertical" flexItem />
+                  <ButtonComponent
+                    label="Register"
+                    buttonVariant="contained"
+                    textColor='primary.contrastText'
+                    hoverTextColor='secondary.main'
+                  />
+                </List>
               </Box>
               <Box
                 sx={{
@@ -136,10 +163,6 @@ const NavBar = ({ isLoggedIn }) => {
                   <ListItem button>
                     <ListItemText primary="About" />
                   </ListItem>
-                  <Divider />
-                  <ListItem button>
-                    <ListItemText primary="Contact Us" />
-                  </ListItem>
                 </List>
               </Box>
             </Drawer>
@@ -155,7 +178,6 @@ const NavBar = ({ isLoggedIn }) => {
             )}
           </Toolbar>
         </AppBar>
-        <Offset />
       </ThemeProvider>
       {/* Content goes here */}
     </>
