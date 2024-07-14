eafce3271c7d5861d0d44ed6936f604e52553295
3Lsome <luckyllemos0909@gmail.com>
2024-07-14 18:29:18 +0800

    Update adjust size for h1 selector for theme

--- src/components/ThemeComponent/ModTheme.jsx
+++ src/components/ThemeComponent/ModTheme.jsx
@@ -27,16 +27,16 @@ const ModTheme = createTheme({
             fontFamily: 'Playfair Display, serif',
             fontSize: '2rem',
             '@media (min-width:400px)': {
-                fontSize: '2.5rem',
+                fontSize: '1.5rem',
             },
             '@media (min-width:600px)': {
-                fontSize: '3rem',
+                fontSize: '2rem',
             },
             '@media (min-width:960px)': {
-                fontSize: '3.5rem',
+                fontSize: '2.5rem',
             },
             '@media (min-width:1280px)': {
-                fontSize: '4rem',
+                fontSize: '3rem',
             },
         },
         body1: {
