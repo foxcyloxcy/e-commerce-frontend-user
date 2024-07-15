34da789ae3ba796612e83bdb1828f0c1fa19a845
3Lsome <luckyllemos0909@gmail.com>
2024-07-16 05:09:52 +0800

    Update use Createroot for rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 // index.js or App.js
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'; // Adjust the path as necessary
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
 );
