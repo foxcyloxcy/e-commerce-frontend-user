6d322c452af4b0b5299814e66db1975e1ab615c6
3Lsome <luckyllemos0909@gmail.com>
2024-07-16 05:10:20 +0800

    Update add parameters to catch routing

--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,19 +1,19 @@
-// ButtonComponent.js
 import React from 'react';
 import Button from '@mui/material/Button';
 import { ThemeProvider } from '@mui/material/styles';
 import ModTheme from '../ThemeComponent/ModTheme';
 
 export default function ButtonComponent(props) {
-    const { label, height, width, textColor, hoverTextColor, buttonVariant } = props;
-
+    const { label, height, width, textColor, hoverTextColor, buttonVariant, component, to } = props;
     return (
         <ThemeProvider theme={ModTheme}>
             <Button
+                component={component}
+                to={to}
                 fullWidth
                 variant={buttonVariant}
-                sx={{ 
-                    mt: 3, 
+                sx={{
+                    mt: 3,
                     mb: 2,
                     color: textColor,
                     '&:hover': {
