d7bf7e3a371b121c0f243a37b17be03165e91eea
3Lsome <luckyllemos0909@gmail.com>
2024-07-15 16:20:37 +0800

    Update implement browse router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// index.js or App.js
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App'; // Adjust the path as necessary
+
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>,
+  document.getElementById('root')
+);
