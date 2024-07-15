36c04a36616373967bc05a94268cbb72f3783c87
3Lsome <luckyllemos0909@gmail.com>
2024-07-15 16:20:50 +0800

    Update implement react-routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/LoginComponent/Login'
 import Register from './components/RegisterComponent/Register'
 import NavBar from './components/NavbarComponent/NavBar'
@@ -10,16 +11,18 @@ import ProductList from './components/ProductListComponent/ProductList';
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
       <NavBar isLoggedIn={isLoggedIn} />
-      <FeaturedHero/>
-      <ProductList/>
-      <Login />
-      <Register />
-      <Footer/>
+      <Routes>
+        <Route path="/" element={<FeaturedHero />} />
+        <Route path="/shop" element={<ProductList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
