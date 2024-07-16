2fbae0e90463134d80a46ae2a0d5b174e7cf4a14
3Lsome <luckyllemos0909@gmail.com>
2024-07-16 08:35:48 +0800

    Add new component HomeComponent

---
+++ src/components/HomeComponent/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FeaturedProducts from '../FeaturedProductsComponent/FeaturedProducts';
+import FeaturedHero from '../HeroComponent/FeaturedHero';
+
+
+export default function Home() {
+
+    return(
+        <>
+        <FeaturedHero/>
+        <FeaturedProducts/>
+        </>
+    )
+}
