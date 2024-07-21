486b3bdcc52aa14068c07a98ef9f431dcd326068
3Lsome <luckyllemos0909@gmail.com>
2024-07-21 18:19:22 +0800

    Update add secure.js

---
+++ src/assets/baseURL/secure.js
@@ -0,0 +1,8 @@
+
+const storageKey = import.meta.env.VITE_SECURE_LOCAL_STORAGE_HASH_KEY;
+const storagePrefix = import.meta.env.VITE_SECURE_LOCAL_STORAGE_PREFIX;
+
+export default ({
+    storageKey,
+    storagePrefix
+})
