22c0c0d2434f0828d6c810e7bf1b8dc5fac4bda4
3Lsome <luckyllemos0909@gmail.com>
2024-07-24 09:32:11 +0800

    Update adjust styling of search bar

--- src/components/ProductListComponent/ProductList.jsx
+++ src/components/ProductListComponent/ProductList.jsx
@@ -70,19 +70,12 @@ const ProductList = (props) => {
     const Search = styled('div')(({ theme }) => ({
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
-            "& .MuiOutlinedInput-notchedOutline" : {
-                borderColor : "red",
-             },
-            "&:hover > .MuiOutlinedInput-notchedOutline" : {
-                borderColor : "lime"
-             },
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.25),
-        },
         marginRight: theme.spacing(2),
         marginLeft: 0,
         width: '100%',
+        border: '1px solid', // Add this line
+        borderColor: theme.palette.secondary.main,
+        backgroundColor: theme.palette.secondary.background,
         [theme.breakpoints.up('md')]: {
             marginLeft: theme.spacing(3),
             width: 'auto',
@@ -97,6 +90,7 @@ const ProductList = (props) => {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        color: theme.palette.secondary.main
     }));
 
     const StyledInputBase = styled(InputBase)(({ theme }) => ({
