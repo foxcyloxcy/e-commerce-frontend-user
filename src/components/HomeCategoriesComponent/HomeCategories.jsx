9b7c8937f83a45c2bb3a384bdd0c0075441d3a3d
3Lsome <luckyllemos0909@gmail.com>
2024-08-05 10:22:03 +0800

    Update fix rendering of sub_categories when user choose a category

--- src/components/HomeCategoriesComponent/HomeCategories.jsx
+++ src/components/HomeCategoriesComponent/HomeCategories.jsx
@@ -1,14 +1,8 @@
-import React, { useState } from 'react';
-import { Grid, Box, Typography, Button, useMediaQuery, useTheme } from '@mui/material';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Grid, Box, Typography, Button, Collapse, Fade, useTheme } from '@mui/material';
 import { styled } from '@mui/system';
-
-const categories = [
-  { label: 'Women', image: 'women.png' },
-  { label: 'Men', image: 'men.png' },
-  { label: 'Baby & Children', image: 'baby.png' },
-  { label: 'Home', image: 'Home.png' },
-  { label: 'Pets', image: 'pets.png' },
-];
+import api from '../../assets/baseURL/api';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const ImageContainer = styled(Box)(({ theme, image }) => ({
   position: 'relative',
@@ -35,7 +29,7 @@ const ImageContainer = styled(Box)(({ theme, image }) => ({
     borderRadius: theme.shape.borderRadius,
   },
   cursor: 'pointer',
-  transition: 'transform 0.3s ease',
+  transition: 'transform 0.5s ease',
   '&:hover': {
     transform: 'scale(1.02)',
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)'
@@ -43,31 +37,80 @@ const ImageContainer = styled(Box)(({ theme, image }) => ({
 }));
 
 const HomeCategories = () => {
+  const [categories, setCategories] = useState([]);
+  const [expandedCategoryId, setExpandedCategoryId] = useState(null);
+  const [viewingSubCategories, setViewingSubCategories] = useState(false);
   const theme = useTheme();
-  const [displayedCategories, setDisplayedCategories] = useState(categories);
+
+  const loadCategories = useCallback(async () => {
+    try {
+      const res = await api.get("api/global/category");
+      if (res.status === 200) {
+        setCategories(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadCategories();
+  }, [loadCategories]);
+
+  const handleCategoryClick = (categoryId) => {
+    setExpandedCategoryId(categoryId);
+    setTimeout(() => setViewingSubCategories(true), 300); // Match this duration with the Fade timeout
+  };
+
+  const handleBackClick = () => {
+    setViewingSubCategories(false);
+    setExpandedCategoryId(null);
+  };
 
   return (
     <Box p={4} mt={10}>
-      <Grid container spacing={2}
-                  display='flex'
-                  justifyContent='center'>
-        {displayedCategories.map((category, index) => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-            key={index}
-          >
-            <ImageContainer image={category.image}>
-              {/* <Typography variant="h6" component="div" sx={{ position: 'relative', zIndex: 1 }}>
-                {category.label}
-              </Typography> */}
-            </ImageContainer>
+      {!viewingSubCategories ? (
+        <Fade in={!viewingSubCategories} timeout={300}>
+          <Grid container spacing={2} display='flex' justifyContent='center'>
+            {categories.map((category) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={category.id}>
+                <ImageContainer
+                  image={category.image}
+                  onClick={() => handleCategoryClick(category.id)}
+                >
+                  <Typography variant="h6" component="div" sx={{ position: 'relative', zIndex: 1 }}>
+                    {category.name}
+                  </Typography>
+                </ImageContainer>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Fade>
+      ) : (
+        <Fade in={viewingSubCategories} timeout={1000}>
+          <Box>
+              <ArrowBackIcon onClick={handleBackClick} sx={{
+                cursor: 'pointer',
+                position: 'absolute'
+              }}/>
+            <Grid container spacing={2} display='flex' justifyContent='center'>
+              {categories
+                .find((category) => category.id === expandedCategoryId)
+                ?.sub_category.map((subCategory) => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={subCategory.id}>
+                <ImageContainer
+                  image={subCategory.image}
+                >
+                  <Typography variant="h6" component="div" sx={{ position: 'relative', zIndex: 1 }}>
+                    {subCategory.name}
+                  </Typography>
+                </ImageContainer>
+                  </Grid>
+                ))}
+            </Grid>
+          </Box>
+        </Fade>
+      )}
     </Box>
   );
 };
