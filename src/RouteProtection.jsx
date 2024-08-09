141638bd2ad7d26a1cbf5dd9f6b3d550bb5296d0
3Lsome <luckyllemos0909@gmail.com>
2024-08-09 10:57:19 +0800

    Update route protection

--- src/RouteProtection.jsx
+++ src/RouteProtection.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export const ProtectedRoute = ({ isLoggedIn, children }) => {
+  const location = useLocation();
   if (!isLoggedIn) {
-    return <Navigate to="/login" />;
+    return <Navigate to={location.pathname} />;
   }
   return children;
 };
 
 export const PublicRoute = ({ isLoggedIn, children }) => {
+  const location = useLocation();
   if (isLoggedIn) {
-    return <Navigate to="/" />;
+    return <Navigate to={location.pathname} />;
   }
   return children;
 };
