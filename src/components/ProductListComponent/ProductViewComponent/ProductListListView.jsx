b0937a9d37c3d0091b0285a6373d9b2fb9c5ff79
3Lsome <luckyllemos0909@gmail.com>
2024-08-10 15:35:04 +0800

    Update move to another folder and adjust styling

--- src/components/ProductListComponent/ProductViewComponent/ProductListListView.jsx
+++ src/components/ProductListComponent/ProductViewComponent/ProductListListView.jsx
@@ -2,15 +2,55 @@ import React from 'react';
 import {
     Grid,
     Typography,
+    FormControl,
+    Select,
+    MenuItem,
+    Tooltip,
+    IconButton,
     Card,
     CardContent,
     Button
 } from '@mui/material';
+import { ChevronLeft, ChevronRight, FavoriteBorder } from '@mui/icons-material';
 import ModTheme from '../../ThemeComponent/ModTheme';
 
-const ProductListListView = ({ productsData }) => {
+const ProductListGrid = ({ productsData, handleProductView  }) => {
+    console.log(handleProductView)
     return (
         <Grid item xs={12} md={8} lg={9}>
+            <header style={{ marginBottom: '20px', paddingBottom: '10px', borderBottom: '1px solid #e0e0e0' }}>
+                <Grid container alignItems="center">
+                <Grid item xs={3} sm={3} md={4}>
+                        <Typography variant="body1">{productsData.length} Items found</Typography>
+                    </Grid>
+                    <Grid item xs={6} sm={6} md={4}>
+                    <FormControl variant="outlined" sx={{minWidth:'100%'}}>
+                            <Select defaultValue="latest">
+                                <MenuItem value="latest">Latest items</MenuItem>
+                                <MenuItem value="trending">Trending</MenuItem>
+                                <MenuItem value="popular">Most Popular</MenuItem>
+                                <MenuItem value="cheapest">Cheapest</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={3} sm={3} md={4} display='flex' justifyContent='center'>
+                        <Tooltip title="List view">
+                            <IconButton onClick={()=>{
+                                handleProductView('list')
+                            }}>
+                                <ChevronLeft />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Grid view">
+                            <IconButton onClick={()=>{
+                                handleProductView('grid')
+                            }}>
+                                <ChevronRight />
+                            </IconButton>
+                        </Tooltip>
+                    </Grid>
+                </Grid>
+            </header>
             {productsData.map((product) => (
                 <Card key={product.id} sx={{ marginBottom: '20px', background: '#fff' }}>
                     <Grid container spacing={0}>
@@ -46,4 +86,4 @@ const ProductListListView = ({ productsData }) => {
     );
 };
 
-export default ProductListListView;
+export default ProductListGrid;
