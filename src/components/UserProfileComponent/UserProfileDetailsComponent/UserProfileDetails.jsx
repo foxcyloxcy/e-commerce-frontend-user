af26e6fb07d8bceacd8471ba1cbcd754f1105b8b
3Lsome <luckyllemos0909@gmail.com>
2024-08-19 10:55:55 +0800

    Update add api call for user and vendor details

--- src/components/UserProfileComponent/UserProfileDetailsComponent/UserProfileDetails.jsx
+++ src/components/UserProfileComponent/UserProfileDetailsComponent/UserProfileDetails.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect, useCallback} from 'react';
 import { Box, Grid, Paper, Avatar, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import ModTheme from '../../ThemeComponent/ModTheme';
+import api from '../../../assets/baseURL/api';
 
 
 const ProfileInfo = styled(Paper)(({ theme }) => ({
@@ -16,15 +17,37 @@ const ProfileInfo = styled(Paper)(({ theme }) => ({
 }));
 
 
-const UserProfileDetails = () => {
+const UserProfileDetails = (props) => {
+    const { userToken } = props
+    const loadProfile = useCallback(async () => {
+        try {
+            const res = await api.get("api/auth/me/profile", {
+                headers: {
+                    Authorization: `Bearer ${userToken}`,
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            if (res.status === 200) {
+                console.log(res.data)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+    
+    useEffect(() => {
+        loadProfile();
+    
+    }, []);
+    
     return (
         <ProfileInfo>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={12}
                     sx={{
                         display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
+                        justifyContent: 'left',
+                        alignItems: 'flex-start',
                         flexDirection: 'column',
                     }}>
                     <Avatar
@@ -33,18 +56,14 @@ const UserProfileDetails = () => {
                         sx={{ width: 150, height: 150 }}
                     />
                     <Typography variant="h6">Annie Stacey</Typography>
-                    <Typography variant="body2">CEO / Co-Founder</Typography>
                 </Grid>
                 <Grid item xs={12} sm={12}>
                     <Typography variant="h6">Profile Information</Typography>
-                    <Typography variant="body2">
-                        Hi, I’m Annie Stacey, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).
-                    </Typography>
                     <Box mt={2}>
-                        <Typography variant="body2">Full Name: Annie Stacey</Typography>
+                        <Typography variant="body2">First Name: Annie</Typography>
+                        <Typography variant="body2">Last Name: Stacey</Typography>
                         <Typography variant="body2">Mobile: (44) 123 1234 123</Typography>
                         <Typography variant="body2">Email: annie@mail.com</Typography>
-                        <Typography variant="body2">Location: UAE</Typography>
                     </Box>
                 </Grid>
             </Grid>
