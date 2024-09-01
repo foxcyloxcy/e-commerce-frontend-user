3c3abc9c6e0d86bbae88026052d50426d60e5986
3Lsome <luckyllemos0909@gmail.com>
2024-09-01 14:38:33 +0800

    Update transfer imageContainer to new folder

--- src/components/HomeCategoriesComponent/HomeCategories.jsx
+++ src/components/HomeCategoriesComponent/HomeCategories.jsx
@@ -1,41 +1,9 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { Grid, Box, Typography, Button, Collapse, Fade, useTheme } from '@mui/material';
-import { styled } from '@mui/system';
+import { Grid, Box, Typography, Fade, useTheme } from '@mui/material';
 import api from '../../assets/baseURL/api';
 import { useNavigate } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
-const ImageContainer = styled(Box)(({ theme, image }) => ({
-  position: 'relative',
-  backgroundImage: `url(${image})`,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  borderRadius: theme.shape.borderRadius,
-  minHeight: 200,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.common,
-  [theme.breakpoints.down('sm')]: {
-    minHeight: 100,
-  },
-  '::after': {
-    content: '""',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    background: 'linear-gradient(to top, rgba(37, 87, 115, 0.6), rgba(37, 87, 115, 0))', //rgba(184, 212, 230, baby blue color
-    borderRadius: theme.shape.borderRadius,
-  },
-  cursor: 'pointer',
-  transition: 'transform 0.5s ease',
-  '&:hover': {
-    transform: 'scale(1.02)',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)'
-  },
-}));
+import ImageContainer from '../ReusableComponents/ImageContainerComponent/ImageContainer';
 
 const HomeCategories = () => {
   const [categories, setCategories] = useState([]);
