b03a9a7d5cce15aff523bb4ccc845a1b4fcef09b
3Lsome <luckyllemos0909@gmail.com>
2024-09-05 20:40:15 +0800

    Update format content for much better readability

--- src/components/PaymentSuccessComponent/PaymentSuccess.jsx
+++ src/components/PaymentSuccessComponent/PaymentSuccess.jsx
@@ -5,42 +5,42 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ModTheme from '../ThemeComponent/ModTheme';
 
 const PaymentSuccess = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleBrowseShop = () => {
-    navigate('/shop');
-  };
+    const handleBrowseShop = () => {
+        navigate('/shop');
+    };
 
-  return (
-    <ThemeProvider theme={ModTheme}>
+    return (
+        <ThemeProvider theme={ModTheme}>
             <Container maxWidth="sm">
-      <Box 
-        display="flex" 
-        flexDirection="column" 
-        alignItems="center" 
-        justifyContent="center" 
-        height="100vh" 
-        textAlign="center"
-      >
-        <CheckCircleIcon sx={{ fontSize: 100, color: 'primary.main' }} />
-        <Typography variant="h4" color="primary" gutterBottom>
-          Payment Successful!
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          Thank you for your purchase. Your payment has been successfully processed.
-        </Typography>
-        <Button 
-          variant="contained" 
-          color="primary" 
-          onClick={handleBrowseShop}
-          sx={{ mt: 4 }}
-        >
-          Browse Shop
-        </Button>
-      </Box>
-    </Container>
-    </ThemeProvider>
-  );
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    height="100vh"
+                    textAlign="center"
+                >
+                    <CheckCircleIcon sx={{ fontSize: 100, color: 'primary.main' }} />
+                    <Typography variant="h4" color="primary" gutterBottom>
+                        Payment Successful!
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        Thank you for your purchase. Your payment has been successfully processed.
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleBrowseShop}
+                        sx={{ mt: 4 }}
+                    >
+                        Browse Shop
+                    </Button>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 };
 
 export default PaymentSuccess;
