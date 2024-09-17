dba47611268e5a987058617f42c4bc8108d82336
3Lsome <luckyllemos0909@gmail.com>
2024-09-17 19:39:15 +0800

    Update update naming of state from data into offers

--- src/components/UserProfileComponent/OffersToMeComponent/ViewOffers.jsx
+++ src/components/UserProfileComponent/OffersToMeComponent/ViewOffers.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { Box, Grid, Typography, Card, CardContent, Button, CardMedia } from '@mui/material';
 import { styled } from '@mui/system';
 import ModTheme from '../../ThemeComponent/ModTheme';
-import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import api from '../../../assets/baseURL/api';
 
 const TruncatedText = styled(Typography)({
@@ -12,10 +12,12 @@ const TruncatedText = styled(Typography)({
 });
 
 
-const ViewOffers = (props) => {
-    const { userToken } = props;
-    const [productsData, setProductsData] = useState([]);
-    const navigate = useNavigate();
+const ViewOffers = () => {
+    const { state } = useLocation();
+    const { product, userToken, userData } = state;
+    const [offersData, setOffersData] = useState([]);
+
+    console.log(product, userToken, userData)
 
     const loadMyOffers = useCallback(async () => {
         try {
@@ -28,7 +30,7 @@ const ViewOffers = (props) => {
 
             if (res.status === 200) {
                 console.log(res.data)
-                setProductsData(res.data.data.data);
+                setOffersData(res.data.data.data);
             }
         } catch (error) {
             console.log(error);
@@ -43,7 +45,7 @@ const ViewOffers = (props) => {
         console.log(product)
     };
 
-    if (!productsData) {
+    if (!offersData) {
         return (
             <Grid container spacing={2} sx={{
                 minHeight: "70vh",
@@ -62,28 +64,28 @@ const ViewOffers = (props) => {
         <Grid container spacing={2} sx={{
             minHeight: "70vh"
         }}>
-            {productsData.map((product) => (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={product.id} style={{ display: 'flex' }}>
+            {offersData.map((offer) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={offer.id} style={{ display: 'flex' }}>
                     <Card sx={{ display: 'flex', flexDirection: 'column', width: '100%', background: '#fff', position: 'relative', height: '450px' }}>
 
-                        {/* Product Image */}
+                        {/* offer Image */}
                         <CardMedia
                             component="img"
                             height="200"
-                            image={product.default_image ? product.default_image.image_url : 'no image available.'}
-                            alt={product.item_name}
+                            image={offer.default_image ? offer.default_image.image_url : 'no image available.'}
+                            alt={offer.item_name}
                             sx={{ objectFit: 'cover' }}
                         />
 
                         {/* Card Content */}
                         <CardContent sx={{ flexGrow: 1 }}>
-                            <TruncatedText variant="h6">{product.item_name}</TruncatedText>
-                            <TruncatedText variant="body2">{product.item_description}</TruncatedText>
+                            <TruncatedText variant="h6">{offer.item_name}</TruncatedText>
+                            <TruncatedText variant="body2">{offer.item_description}</TruncatedText>
                             <Typography variant="h6" sx={{ marginTop: '10px' }}>
-                                AED {product.price}
+                                AED {offer.price}
                             </Typography>
                             <Typography variant="body2" sx={{ color: ModTheme.palette.primary.light }}>
-                                {product.is_bid ? 'You are accepting offers' : ''}
+                                {offer.is_bid ? 'You are accepting offers' : ''}
                             </Typography>
                         </CardContent>
 
@@ -93,7 +95,7 @@ const ViewOffers = (props) => {
                                 variant="contained"
                                 color="primary"
                                 fullWidth
-                                onClick={() => handleDetailsClick(product)}
+                                onClick={() => handleDetailsClick(offer)}
                             >
                                 View offers
                             </Button>
