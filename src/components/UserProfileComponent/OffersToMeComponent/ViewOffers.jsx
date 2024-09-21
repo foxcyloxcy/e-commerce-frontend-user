625acf3eb310360370db74049fe61e1d702629f3
3Lsome <luckyllemos0909@gmail.com>
2024-09-21 16:09:05 +0800

    Update fix handleOffers function

--- src/components/UserProfileComponent/OffersToMeComponent/ViewOffers.jsx
+++ src/components/UserProfileComponent/OffersToMeComponent/ViewOffers.jsx
@@ -4,6 +4,7 @@ import { styled } from '@mui/system';
 import ModTheme from '../../ThemeComponent/ModTheme';
 import { useNavigate, useLocation } from 'react-router-dom';
 import api from '../../../assets/baseURL/api';
+import Swal from 'sweetalert2';
 
 const TruncatedText = styled(Typography)({
     whiteSpace: 'nowrap',
@@ -25,14 +26,14 @@ const StatusBadge = styled(Box)(({ theme, status }) => ({
 
 const ViewOffers = () => {
     const { state } = useLocation();
-    const { userToken, product } = state;
-    console.log(product, userToken)
+    const { productUuid, userToken } = state;
     const [productsData, setProductsData] = useState([]);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const loadOffersToMe = useCallback(async () => {
         try {
-            const res = await api.get(`/api/auth/me/offers-to-me`, {
+            const res = await api.get(`/api/auth/me/offers-to-me/${productUuid}`, {
                 headers: {
                     Authorization: `Bearer ${userToken}`,
                     'Content-Type': 'multipart/form-data',
@@ -41,7 +42,7 @@ const ViewOffers = () => {
 
             if (res.status === 200) {
                 console.log(res.data)
-                setProductsData(res.data.data);
+                setProductsData(res.data.offers);
             }
         } catch (error) {
             console.log(error);
@@ -52,11 +53,64 @@ const ViewOffers = () => {
         loadOffersToMe();
     }, [loadOffersToMe]);
 
-    const handleViewOffers = (product) => {
+    const handleAcceptOffer = async (productId) => {
+
+        try {
+            Swal.fire({
+                title: 'Wait!',
+                text: 'Are you sure you want to accept this offer?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: 'Yes',
+                confirmButtonColor: ModTheme.palette.primary.main,
+                cancelButtonText: 'Cancel'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    const res = api.put(`/api/auth/me/offers-to-me/accept/${productId}`, '', {
+                        headers: {
+                          Authorization: `Bearer ${userToken}`,
+                          'Content-Type': 'multipart/form-data',
+                        },
+                      });
+                
+                      if (res.status === 200) {
+                        console.log(res.data)
+                        const successMessage = res.data.message;
+                        Swal.fire({
+                            title: 'Great!',
+                            text: successMessage,
+                            icon: 'success',
+                            showCancelButton: true,
+                            confirmButtonText: 'Ok',
+                            confirmButtonColor: ModTheme.palette.primary.main,
+                            cancelButtonText: 'Cancel'
+                          }).then((result) => {
+                            if (result.isConfirmed) {
+                                navigate('/my-profile');
+                            }
+                          })
+                      }
+                }
+              });
+
+          } catch (error) {
+            console.log(error);
+            Swal.fire({
+              title: 'Error!',
+              text: error,
+              icon: 'error',
+            });
+          } finally {
+            setLoading(false);
+          }
+    };
+
+    const handleRejectOffer = (product) => {
+        //put
         navigate('/view-offers', { state: { product } });
     };
 
-    if (!product || !userToken) {
+    if (!productUuid || !userToken) {
         return (
             <Grid container spacing={2} sx={{
                 minHeight: "70vh",
@@ -75,15 +129,11 @@ const ViewOffers = () => {
         <ThemeProvider theme={ModTheme}>
             <Grid container spacing={2} sx={{ mt: 10, mb: 10, pl: 2, pr: 2 }}>
                 <Grid item xs={12}>
-                    <Typography variant="h6">Available offers for {product.item_name}</Typography>
+                    <Typography variant="h6">Available offers for</Typography>
                 </Grid>
                 {productsData.map((product) => (
                     <Grid item xs={12} sm={6} md={4} lg={3} key={product.id} style={{ display: 'flex' }}>
                         <Card sx={{ display: 'flex', flexDirection: 'column', width: '100%', background: '#fff', position: 'relative', height: '450px' }}>
-                            {/* Status Badge */}
-                            <StatusBadge status={product.status}>
-                                Available offers(${product.offers})
-                            </StatusBadge>
 
                             {/* Product Image */}
                             <CardMedia
@@ -115,7 +165,7 @@ const ViewOffers = () => {
                                         backgroundColor: ModTheme.palette.primary.main
                                     }}
                                     fullWidth
-                                    onClick={() => handleViewOffers(product)}
+                                    onClick={() => handleAcceptOffer(product.id)}
                                 >
                                     Accept
                                 </Button>
@@ -126,7 +176,7 @@ const ViewOffers = () => {
                                         backgroundColor: ModTheme.palette.secondary.main
                                     }}
                                     fullWidth
-                                    onClick={() => handleViewOffers(product)}
+                                    onClick={() => handleRejectOffer(product.id)}
                                 >
                                     Reject
                                 </Button>
