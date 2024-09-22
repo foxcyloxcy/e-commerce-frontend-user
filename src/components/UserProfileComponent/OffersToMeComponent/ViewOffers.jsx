5d508e8915946b34eef7b22dfea4f94cbc2a6063
3Lsome <luckyllemos0909@gmail.com>
2024-09-23 06:47:30 +0800

    Update fix ui for view offers, use listing ui

--- src/components/UserProfileComponent/OffersToMeComponent/ViewOffers.jsx
+++ src/components/UserProfileComponent/OffersToMeComponent/ViewOffers.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Box, Grid, Typography, Card, CardContent, Button, CardMedia, ThemeProvider } from '@mui/material';
+import { Box, Grid, Typography, Card, CardContent, Button, ThemeProvider } from '@mui/material';
 import { styled } from '@mui/system';
 import ModTheme from '../../ThemeComponent/ModTheme';
 import { useNavigate, useLocation } from 'react-router-dom';
@@ -12,23 +12,10 @@ const TruncatedText = styled(Typography)({
     textOverflow: 'ellipsis',
 });
 
-const StatusBadge = styled(Box)(({ theme, status }) => ({
-    position: 'absolute',
-    top: theme.spacing(1),
-    left: theme.spacing(1),
-    backgroundColor: status === 0 ? '#ff9800' : status === 1 ? '#4caf50' : '#f44336', // Orange for pending, green for approved, red for rejected
-    color: '#fff',
-    borderRadius: '4px',
-    padding: '2px 8px',
-    fontSize: '12px',
-    fontWeight: 'bold',
-}));
-
 const ViewOffers = () => {
     const { state } = useLocation();
     const { productUuid, userToken } = state;
     const [productsData, setProductsData] = useState([]);
-    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const loadOffersToMe = useCallback(async () => {
@@ -41,20 +28,19 @@ const ViewOffers = () => {
             });
 
             if (res.status === 200) {
-                console.log(res.data)
+                console.log('data offers',res.data.offers)
                 setProductsData(res.data.offers);
             }
         } catch (error) {
             console.log(error);
         }
-    }, [userToken]);
+    }, [userToken, productUuid]);
 
     useEffect(() => {
         loadOffersToMe();
     }, [loadOffersToMe]);
 
     const handleAcceptOffer = async (productId) => {
-
         try {
             Swal.fire({
                 title: 'Wait!',
@@ -64,123 +50,81 @@ const ViewOffers = () => {
                 confirmButtonText: 'Yes',
                 confirmButtonColor: ModTheme.palette.primary.main,
                 cancelButtonText: 'Cancel'
-              }).then((result) => {
+            }).then((result) => {
                 if (result.isConfirmed) {
                     const res = api.put(`/api/auth/me/offers-to-me/accept/${productId}`, '', {
                         headers: {
-                          Authorization: `Bearer ${userToken}`,
-                          'Content-Type': 'multipart/form-data',
+                            Authorization: `Bearer ${userToken}`,
+                            'Content-Type': 'multipart/form-data',
                         },
-                      });
-                
-                      if (res.status === 200) {
-                        console.log(res.data)
+                    });
+                    console.log(res)
+                    if (res.status === 200) {
                         const successMessage = res.data.message;
                         Swal.fire({
                             title: 'Great!',
                             text: successMessage,
                             icon: 'success',
-                            showCancelButton: true,
                             confirmButtonText: 'Ok',
                             confirmButtonColor: ModTheme.palette.primary.main,
-                            cancelButtonText: 'Cancel'
-                          }).then((result) => {
-                            if (result.isConfirmed) {
-                                navigate('/my-profile');
-                            }
-                          })
-                      }
+                        }).then(() => {
+                            navigate('/my-profile');
+                        });
+                    }
                 }
-              });
+            });
 
-          } catch (error) {
+        } catch (error) {
             console.log(error);
             Swal.fire({
-              title: 'Error!',
-              text: error,
-              icon: 'error',
+                title: 'Error!',
+                text: error.message,
+                icon: 'error',
             });
-          } finally {
-            setLoading(false);
-          }
+        }
     };
 
-    const handleRejectOffer = (product) => {
-        //put
-        navigate('/view-offers', { state: { product } });
+    const handleRejectOffer = (productId) => {
+        // Reject offer functionality
+        navigate('/view-offers', { state: { productId } });
     };
-
-    if (!productUuid || !userToken) {
-        return (
-            <Grid container spacing={2} sx={{
-                minHeight: "70vh",
-                mt: 5,
-            }}>
-                <Grid item>
-                    <Typography>
-                        Loading...
-                    </Typography>
-                </Grid>
-            </Grid>
-        )
-    }
 
     return (
         <ThemeProvider theme={ModTheme}>
-            <Grid container spacing={2} sx={{ mt: 10, mb: 10, pl: 2, pr: 2 }}>
-                <Grid item xs={12}>
-                    <Typography variant="h6">Available offers for</Typography>
-                </Grid>
+            <Grid container spacing={2} sx={{ padding: '20px', mt: 10, minHeight: '70vh', width: '100%' }}>
                 {productsData.map((product) => (
-                    <Grid item xs={12} sm={6} md={4} lg={3} key={product.id} style={{ display: 'flex' }}>
-                        <Card sx={{ display: 'flex', flexDirection: 'column', width: '100%', background: '#fff', position: 'relative', height: '450px' }}>
-
-                            {/* Product Image */}
-                            <CardMedia
-                                component="img"
-                                height="200"
-                                image={product.default_image ? product.default_image.image_url : 'no image available.'}
-                                alt={product.item_name}
-                                sx={{ objectFit: 'cover' }}
-                            />
-
-                            {/* Card Content */}
-                            <CardContent sx={{ flexGrow: 1 }}>
-                                <TruncatedText variant="h6">{product.item_name}</TruncatedText>
-                                <TruncatedText variant="body2">{product.item_description}</TruncatedText>
-                                <Typography variant="h6" sx={{ marginTop: '10px' }}>
-                                    AED {product.price}
-                                </Typography>
-                                <Typography variant="body2" sx={{ color: ModTheme.palette.primary.light }}>
-                                    {product.is_bid ? 'You are accepting offers' : ''}
-                                </Typography>
-                            </CardContent>
-
-                            {/* Details Button */}
-                            <CardContent sx={{ position: 'absolute', width: '100%', top: '84%', display: 'flex' }}>
-                                <Button
-                                    variant="contained"
-                                    sx={{
-                                        marginTop: 1, marginBottom: 1, marginRight: 1,
-                                        backgroundColor: ModTheme.palette.primary.main
-                                    }}
-                                    fullWidth
-                                    onClick={() => handleAcceptOffer(product.id)}
-                                >
-                                    Accept
-                                </Button>
-                                <Button
-                                    variant="contained"
-                                    sx={{
-                                        marginTop: 1, marginBottom: 1, marginLeft: 1,
-                                        backgroundColor: ModTheme.palette.secondary.main
-                                    }}
-                                    fullWidth
-                                    onClick={() => handleRejectOffer(product.id)}
-                                >
-                                    Reject
-                                </Button>
-                            </CardContent>
+                    <Grid item xs={12} md={12} lg={12} key={product.id}>
+                        <Card sx={{ position: 'relative', background: '#fff', width: '100%' }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={10}>
+                                    <Box sx={{ position: 'relative', height: '100%', padding: 1 }}>
+                                    <Typography variant="h6">Email: {product.buyer.email}</Typography>
+                                    <Typography variant="h6">Full Name: {product.buyer.first_name+' '+product.buyer.last_name }</Typography>
+                                    <Typography variant="h6">Item original price: AED {product.item.price}</Typography>
+                                    <Typography variant="h6">Offer: AED {product.asking_price}</Typography>
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={12} sm={2}>
+                                    <CardContent>
+                                        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginTop: '10px' }}>
+                                            <Button
+                                                variant="contained"
+                                                sx={{ backgroundColor: ModTheme.palette.primary.main }}
+                                                onClick={() => handleAcceptOffer(product.id)}
+                                            >
+                                                Accept
+                                            </Button>
+                                            <Button
+                                                variant="contained"
+                                                sx={{ backgroundColor: ModTheme.palette.secondary.main, marginTop: 1 }}
+                                                onClick={() => handleRejectOffer(product.id)}
+                                            >
+                                                Reject
+                                            </Button>
+                                        </Box>
+                                    </CardContent>
+                                </Grid>
+                            </Grid>
                         </Card>
                     </Grid>
                 ))}
