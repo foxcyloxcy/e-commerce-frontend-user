ffc415c773985f401b9451f788dc37c137e0e593
3Lsome <luckyllemos0909@gmail.com>
2024-09-24 21:27:28 +0800

    Update adjust ui of textfield and buttons

--- src/components/ForgotPasswordComponent/CreateNewPassword.jsx
+++ src/components/ForgotPasswordComponent/CreateNewPassword.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { TextField, Button, Box, IconButton, InputAdornment, ThemeProvider } from '@mui/material';
-import ModTheme from '../../ThemeComponent/ModTheme';
+import { TextField, Button, Box, IconButton, InputAdornment, Grid, ThemeProvider } from '@mui/material';
+import ModTheme from '../ThemeComponent/ModTheme';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import api from '../../../assets/baseURL/api';
+import api from '../../assets/baseURL/api';
 import Swal from 'sweetalert2';
 
-const ChangePassword = (props) => {
+const CreateNewPassword = (props) => {
   const { userToken } = props;
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -18,8 +18,12 @@ const ChangePassword = (props) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const url = `/api/auth/me/change-password?password=${newPassword}&password_confirmation=${confirmPassword}`;
-      const res = await api.put(url, {}, {
+      const url = `/api/auth/me/set-forgot-password`;
+      const res = await api.put(url, {
+        uuid: uuId,
+        password: newPassword,
+        password_confirmation: confirmPassword
+      }, {
         headers: {
           Authorization: `Bearer ${userToken}`,
           'Content-Type': 'application/json',
@@ -27,92 +31,97 @@ const ChangePassword = (props) => {
       });
 
       if (res.status === 200) {
-        const successMessage = res.data.message
+        const successMessage = res.data.message;
         Swal.fire({
           title: successMessage,
           icon: 'success',
           confirmButtonText: 'Ok',
           confirmButtonColor: ModTheme.palette.primary.main,
-        })
-        setNewPassword("")
-        setConfirmPassword("")
+        });
+        setNewPassword("");
+        setConfirmPassword("");
       }
     } catch (error) {
       console.log("Error saving profile data:", error);
 
-      if(error.response.data.message.password){
-        const errorMessage = error.response.data.message.password[0]
+      if (error.response.data.message.password) {
+        const errorMessage = error.response.data.message.password[0];
         Swal.fire({
           title: errorMessage,
           icon: 'error',
           confirmButtonText: 'Ok',
           confirmButtonColor: ModTheme.palette.primary.main,
-        })
+        });
       }
     }
   };
 
-
   return (
     <ThemeProvider theme={ModTheme}>
-      <Box component="form" onSubmit={handleSubmit} sx={{ ml: 5, mt: 4 }}>
-        <TextField
-          label="New Password"
-          type={showNewPassword ? 'text' : 'password'}
-          fullWidth
-          margin="normal"
-          size="small"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowNewPassword(!showNewPassword)}
-                  edge="end"
-                >
-                  {showNewPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <TextField
-          label="Confirm New Password"
-          type={showConfirmPassword ? 'text' : 'password'}
-          fullWidth
-          margin="normal"
-          size="small"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                  edge="end"
-                >
-                  {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          fullWidth
-          disabled={isSaveDisabled}
-          sx={{ mt: 2 }}
-        >
-          Save
-        </Button>
+      <Box component="form" onSubmit={handleSubmit} sx={{mt: 10, pl: 5, pr: 5, minHeight: '60vh' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="New Password"
+              type={showNewPassword ? 'text' : 'password'}
+              fullWidth
+              size="small"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowNewPassword(!showNewPassword)}
+                      edge="end"
+                    >
+                      {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Confirm New Password"
+              type={showConfirmPassword ? 'text' : 'password'}
+              fullWidth
+              size="small"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                      edge="end"
+                    >
+                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={isSaveDisabled}
+              sx={{ mt: 2 }}
+            >
+              Save
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
     </ThemeProvider>
   );
 };
 
-export default ChangePassword;
+export default CreateNewPassword;
