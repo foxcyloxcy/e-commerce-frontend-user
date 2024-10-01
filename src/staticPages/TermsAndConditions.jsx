bc0ab8dd608d9dc1bd126a4c7132a9aefc006c94
3Lsome <luckyllemos0909@gmail.com>
2024-10-01 16:14:54 +0800

    Update import components to render

--- src/staticPages/TermsAndConditions.jsx
+++ src/staticPages/TermsAndConditions.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Container, Typography, Box, ThemeProvider, Paper } from '@mui/material';
 import ModTheme from '../components/ThemeComponent/ModTheme';
+import BuyerAndSellerTerms from './BuyerAndSellerTermsComponent/BuyerAndSellerTerms';
+import CookiesPolicy from './CookiePolicyComponent/CookiesPolicy';
+import DataProcessingAgreement from './DataProcessingComponent/DataProcessing';
+import PrivacyPolicy from './PrivacyPolicyComponent/PrivacyPolicy';
+import RefundPolicy from './RefundPolicyComponent/RefundPolicy';
+import ShippingAndDelivery from './ShippingAndDeliveryComponent/ShippingAndDelivery';
+import TermsOfUse from './TermsOfUseComponent/TermsOfUse';
 
 const TermsAndConditions = () => {
     return (
@@ -8,13 +15,22 @@ const TermsAndConditions = () => {
             <Container sx={{
                 marginTop: 13,
                 marginBottom: 10,
+                minHeight: '60vh',
                 maxWidth: { xs: 'sm', sm: 'md', md: 'xl', lg: 'xl', xl: 'xl' },
             }}>
                 <Paper elevation={3} sx={{ padding: 4, background: '#fff', width: '100%' }}>
                     <Box>
-                        <Typography variant="h2" align="center" gutterBottom>
+                        <Typography variant="h4" align="center" gutterBottom>
                             TERMS AND CONDITIONS
                         </Typography>
+
+                        <TermsOfUse/>
+                        <BuyerAndSellerTerms/>
+                        <DataProcessingAgreement/>
+                        <PrivacyPolicy/>
+                        <RefundPolicy/>
+                        <ShippingAndDelivery/>
+                        <CookiesPolicy/>
 
                     </Box>
                 </Paper>
