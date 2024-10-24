979bcd81c0af83a6e6ed12e1567eb4509a45e123
3Lsome <luckyllemos0909@gmail.com>
2024-10-24 09:22:23 +0800

    Update modify card ui, add chat to seller button

--- src/components/UserProfileComponent/MyItemPurchaseComponent/MyItemPurchase.jsx
+++ src/components/UserProfileComponent/MyItemPurchaseComponent/MyItemPurchase.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Box, Grid, Typography, Card, CardContent, Button, CardMedia } from '@mui/material';
+import { Box, Grid, Typography, Card, CardContent, Divider, Button } from '@mui/material';
 import { styled } from '@mui/system';
-import ModTheme from '../../ThemeComponent/ModTheme';
 import { useNavigate } from 'react-router-dom';
 import api from '../../../assets/baseURL/api';
-import PriceBreakdownModal from '../../ReusableComponents/ModalComponent/PriceBreakDownModal';
-import MapViewModal from '../../ReusableComponents/ModalComponent/MapViewModal';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const TruncatedText = styled(Typography)({
     whiteSpace: 'nowrap',
@@ -13,16 +11,11 @@ const TruncatedText = styled(Typography)({
     textOverflow: 'ellipsis',
 });
 
-
 const MyItemPurchase = (props) => {
     const { userToken, fromParentUserData } = props;
     const [productsData, setProductsData] = useState([]);
     const navigate = useNavigate();
-    const userData = fromParentUserData
-    const [openPriceBreakdownModal, setOpenPriceBreakdownModal] = useState(false);
-    const [openMap, setOpenMap] = useState(false);
-    const [selectedProduct, setSelectedProduct] = useState(null);
-    const [selectedAddress, setSelectedAddress] = useState(null);
+    const userData = fromParentUserData;
 
     const loadMyItemPurchase = useCallback(async () => {
         try {
@@ -32,9 +25,8 @@ const MyItemPurchase = (props) => {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-            
-            console.log(res.data)
             if (res.status === 200) {
+                console.log(res.data)
                 setProductsData(res.data.offers.data);
             }
         } catch (error) {
@@ -50,45 +42,25 @@ const MyItemPurchase = (props) => {
         navigate('/my-item-purchase-details', { state: { productUuid, userToken, userData } });
     };
 
-    const handleOpenPriceBreakdown = (product) => {
-        setSelectedProduct(product);
-        setOpenPriceBreakdownModal(true);
-    };
-
-    const handleClosePriceBreakdown = () => {
-        setOpenPriceBreakdownModal(false);
-        setSelectedProduct(null);
-    }
-
-    const handleOpenMap = (address) => {
-        setSelectedAddress(address);
-        setOpenMap(true);
-    };
-
-    const handleCloseMap = () => {
-        setOpenMap(false);
-        setSelectedAddress(null);
-    };
 
     const formatPrice = (price) => {
         return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     };
 
-    const parseAddress = (address) => {
-        const objectAddress = JSON.parse(address)
-        // console.log(objectAddress)
-        let addressName = ""
-
-        if (objectAddress[1]) {
-            addressName = objectAddress[1].name
-        }
-        return addressName.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    const handleChatSeller = (userMobileNo) => {
+        
+        // Remove '+' and spaces from the mobile number
+        const filteredMobileNo = userMobileNo.replace(/[+ ]/g, '');
+    
+        const userWhatsApp = `https://wa.me/${filteredMobileNo}`;
+    
+        window.open(userWhatsApp, '_blank');
     };
 
     if (!productsData || productsData.length <= 0) {
         return (
             <Grid container spacing={2} marginLeft={5}>
-                You got no item purchased.
+                You have no item purchased.
             </Grid>
         );
     }
@@ -96,78 +68,65 @@ const MyItemPurchase = (props) => {
     return (
         <Grid container spacing={2}>
             {productsData.map((product) => (
-                <Grid item xs={6} sm={6} md={4} lg={3} key={product.id} style={{ display: 'flex' }}>
-                    <Card sx={{ display: 'flex', flexDirection: 'column', width: '100%', background: '#fff', position: 'relative', height: '450px' }}>
+                <Grid item xs={12} sm={6} md={6} lg={4} key={product.id}>
+                    <Card sx={{ padding: '16px', background: '#f8f8f8', border: '1px solid #ddd', borderRadius: '8px', height: '500px', position: 'relative', }}>
+                        {/* Transaction Header */}
+                        <Typography variant="h6" fontWeight="bold" gutterBottom>
+                            Transaction #{product.transaction_number}
+                        </Typography>
 
-                        {/* Product Image */}
-                        <CardMedia
-                            component="img"
-                            height="200"
-                            image={product.default_image ? product.default_image.image_url : 'no image available.'}
-                            alt={product.item_name}
-                            sx={{ objectFit: 'cover' }}
-                        />
+                        <Divider />
 
-                        {/* Card Content */}
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <TruncatedText variant="h6">{product.item_name}</TruncatedText>
-                            <TruncatedText variant="body2">{product.item_description}</TruncatedText>
-                            {
-                                    product.address && (
-                                        <Typography 
-                                            variant="body1" 
-                                            sx={{ marginTop: '10px', cursor: 'pointer', textDecoration: 'underline' }}
-                                            onClick={() => handleOpenMap(product.address)}>
-                                                Collection {parseAddress(product.address)}
-                                        </Typography>
-                                    )
-                                }
-                                <Typography
-                                    variant="body1"
-                                    color="primary"
-                                    onClick={() => handleOpenPriceBreakdown(product.my_offer)}
-                                    sx={{ cursor: 'pointer', textDecoration: 'underline' }}
-                                >
-                                    AED {formatPrice(product.my_offer.asking_price)}
-                                </Typography>
+                        {/* Item Details */}
+                        <CardContent>
+                            <Typography variant="subtitle1" fontWeight="bold">
+                                Item: {product.item_name}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                Sold by: {product.seller.first_name} {product.seller.last_name}
+                            </Typography>
                         </CardContent>
 
-                        {/* Details Button */}
-                        <CardContent sx={{ position: 'absolute', width: '100%', top: '84%' }}>
-                            {
-                                product.my_offer.is_accepted === 1 && (
-                                    <Button
-                                        variant="contained"
-                                        color="primary"
-                                        fullWidth
-                                        sx={{ marginTop: '10px' }}
-                                        onClick={() => handleDetailsClick(product.uuid)}
-                                    >
-                                        View Details
-                                    </Button>
-                                )
-                            }
+                        <Divider />
+
+                        {/* Price Breakdown */}
+                        <CardContent>
+                            <Typography variant="body2">Subtotal: AED {formatPrice(product.subtotal_amount)}</Typography>
+                            <Typography variant="body2">Service Fee: AED {formatPrice(product.service_fee_amount)}</Typography>
+                            <Typography variant="body2">Discount: AED {formatPrice(product.discount_amount)}</Typography>
+                            <Typography variant="h6" fontWeight="bold">
+                                Total: AED {formatPrice(product.total_amount)}
+                            </Typography>
+                        </CardContent>
+                        <Divider />
+                        <CardContent sx={{ position: 'absolute', width: '100%', top: '70%' }}>
+                        <Grid item>
+                        <Button
+                                variant="contained"
+                                color="primary"
+                                fullWidth
+                            startIcon={<WhatsAppIcon />}
+                            onClick={() => handleChatSeller(product.seller.mobile_number)}
+                        >
+                            Chat seller
+                        </Button>
+                        </Grid>
+                        {/* View Details Button */}
+                        <Box sx={{ textAlign: 'center' }}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                fullWidth
+                                sx={{marginTop: 1}}
+                                onClick={() => handleDetailsClick(product.uuid)}
+                            >
+                                View Item Details
+                            </Button>
+                        </Box>
                         </CardContent>
                     </Card>
                 </Grid>
             ))}
-
-            {/* Reuse PriceBreakdownModal */}
-            {selectedProduct && (
-                <PriceBreakdownModal
-                    open={openPriceBreakdownModal}
-                    onClose={handleClosePriceBreakdown}
-                    product={selectedProduct}
-                />
-            )}
-
-            {selectedAddress && (
-                <MapViewModal
-                    open={openMap}
-                    onClose={handleCloseMap}
-                    address={selectedAddress}
-                />
-            )}
         </Grid>
     );
 };
