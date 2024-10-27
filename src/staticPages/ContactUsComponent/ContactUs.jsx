823e75e9f66f16a44cabebed754c216fb59c2f53
3Lsome <luckyllemos0909@gmail.com>
2024-10-27 20:28:45 +0800

    Update implement modtheme

--- src/staticPages/ContactUsComponent/ContactUs.jsx
+++ src/staticPages/ContactUsComponent/ContactUs.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Box, Typography, List, ListItem, Link, Paper, Container } from '@mui/material';
+import { Box, Typography, List, ListItem, Link, Paper, Container, ThemeProvider } from '@mui/material';
+import ModTheme from '../../components/ThemeComponent/ModTheme';
 
 const ContactUs = () => {
   return (
-    <Container sx={{
-      marginTop: 15,
-      marginBottom: 10,
-      maxWidth: { xs: 'sm', sm: 'md', md: 'xl', lg: 'xl', xl: 'xl' },
-    }}>
-      <Paper elevation={3} sx={{ padding: 4, mt: 5, background: '#fff' }}>
+    <ThemeProvider theme={ModTheme}>
+      <Container sx={{
+        marginTop: 15,
+        marginBottom: 10,
+        maxWidth: { xs: 'sm', sm: 'md', md: 'xl', lg: 'xl', xl: 'xl' },
+      }}>
+        <Paper elevation={3} sx={{ padding: 4, mt: 5, background: '#fff' }}>
 
-        <Typography variant="h4" gutterBottom>
-          Contact Us
-        </Typography>
-        <Typography variant="body1" paragraph>
+          <Typography variant="h4" gutterBottom>
+            Contact Us
+          </Typography>
+          <Typography variant="body1" paragraph>
             Email us at <Link href="mailto:hello@therelovedmarketplace.com">hello@therelovedmarketplace.com</Link>
-        </Typography>
-      </Paper>
-    </Container>
+          </Typography>
+        </Paper>
+      </Container>
+    </ThemeProvider>
 
   );
 };
