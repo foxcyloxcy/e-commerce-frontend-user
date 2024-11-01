80aa2dc87e1162b5f5b8977f8b60164097b9eed4
3Lsome <luckyllemos0909@gmail.com>
2024-11-01 14:30:54 +0800

    Update add prompt if the user has no bank details

--- src/components/HomeComponent/Home.jsx
+++ src/components/HomeComponent/Home.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import FeaturedProducts from '../FeaturedProductsComponent/FeaturedProducts';
 import FeaturedHero from '../HeroComponent/FeaturedHero';
 import HomeCategories from '../HomeCategoriesComponent/HomeCategories';
+import Swal from 'sweetalert2';
+import ModTheme from '../ThemeComponent/ModTheme';
 
 
 export default function Home(props) {
-    const { parentIsLoggedIn } = props
-    return(
+    const { parentIsLoggedIn, userData } = props
+    const history = useNavigate();
+
+    useEffect(() => {
+        if(userData){
+            const parsedUserData = JSON.parse(userData)
+            if(parsedUserData.has_bank_details === "No"){
+                Swal.fire({
+                    title: 'Read me',
+                    text: "Please visit your My Profile page and create your bank details. You are required to add bank details before you can post an item.",
+                    icon: 'warning',
+                    confirmButtonText: 'Go to My Profile',
+                    confirmButtonColor: ModTheme.palette.primary.main,
+                    showCancelButton: true,
+                    cancelButtonText: 'I will do it later'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        history("/my-profile");
+                    }
+                })
+            }
+        }
+
+    }, [userData])
+    return (
         <>
-        <FeaturedHero parentIsLoggedIn={parentIsLoggedIn}/>
-        <HomeCategories/>
-        <FeaturedProducts/>
+            <FeaturedHero parentIsLoggedIn={parentIsLoggedIn} />
+            <HomeCategories />
+            <FeaturedProducts />
         </>
     )
 }
