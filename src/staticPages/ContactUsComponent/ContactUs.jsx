efd7236a455196025ddc03fab5f134b5d7520640
3Lsome <luckyllemos0909@gmail.com>
2025-02-18 10:47:30 +0800

    Update add whatsapp contact

--- src/staticPages/ContactUsComponent/ContactUs.jsx
+++ src/staticPages/ContactUsComponent/ContactUs.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box, Typography, List, ListItem, Link, Paper, Container, ThemeProvider } from '@mui/material';
 import ModTheme from '../../components/ThemeComponent/ModTheme';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const ContactUs = () => {
   return (
@@ -15,9 +17,29 @@ const ContactUs = () => {
           <Typography variant="h4" gutterBottom>
             Contact Us
           </Typography>
-          <Typography variant="body1" paragraph>
-            Email us at <Link href="mailto:hello@therelovedmarketplace.com">hello@therelovedmarketplace.com</Link>
+
+          <Typography variant="body1" paragraph 
+            sx={{
+              display: 'flex',
+            }}
+            >
+            <MailOutlineIcon/> Email us at <Link sx={{
+              paddingLeft: 0.8
+            }} href="mailto:hello@therelovedmarketplace.com"> hello@therelovedmarketplace.com</Link>
           </Typography>
+
+          <Typography variant="body1" paragraph
+            sx={{
+              display: 'flex',
+            }}
+            >
+          <WhatsAppIcon/> Send us a message at 
+            <Link sx={{
+              paddingLeft: 0.8
+            }} href="https://wa.me/+971561748641" target="_blank" rel="noopener noreferrer"> WhatsApp
+            </Link>
+          </Typography>
+          
         </Paper>
       </Container>
     </ThemeProvider>
