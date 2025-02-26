f052688597c1e933341fcdee18af173bc9edd09e
3Lsome <luckyllemos0909@gmail.com>
2025-02-26 12:24:39 +0800

    Update adjust url for going to shop

--- src/components/HomeCategoriesComponent/HomeCategories.jsx
+++ src/components/HomeCategoriesComponent/HomeCategories.jsx
@@ -33,7 +33,7 @@ const HomeCategories = () => {
   };
 
   const routeToProductList = (subCategoryId) => {
-    history(`/shop?page=&size=&sort=&sub_category_id=${subCategoryId}&filter[min_price]=&filter[max_price]=&filter[keyword]=&filter[properties]=&`);
+    history(`/shop?page=&size=&sort=&sub_category_id=${subCategoryId}&filter_min_price=&filter_max_price=&filter_keyword=&filter_properties=&`);
   };
 
 
