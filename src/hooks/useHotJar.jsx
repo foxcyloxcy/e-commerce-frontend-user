7d1f1f81a6a0c4f72bb8982e1eca4ffa996ca408
3Lsome <luckyllemos0909@gmail.com>
2025-04-15 14:36:28 +0800

    Update add routing trigger for useHotjar.js component

--- src/hooks/useHotJar.jsx
+++ src/hooks/useHotJar.jsx
@@ -1,16 +1,31 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const useHotjar = () => {
+  const location = useLocation();
+
   useEffect(() => {
-    (function(h, o, t, j, a, r) {
-      h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
-      h._hjSettings = { hjid: 6372055, hjsv: 6 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script'); r.async = 1;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    // Initialize Hotjar only once on first mount
+    if (!window.hj) {
+      (function(h, o, t, j, a, r) {
+        h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
+        h._hjSettings = { hjid: 6372055, hjsv: 6 };
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script'); r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    }
   }, []);
+
+  useEffect(() => {
+    // Notify Hotjar of route changes in SPA
+    console.log(window.hj)
+    console.log(window.hj('stateChange', location.pathname))
+    if (window.hj) {
+      window.hj('stateChange', location.pathname);
+    }
+  }, [location]);
 };
 
 export default useHotjar;
