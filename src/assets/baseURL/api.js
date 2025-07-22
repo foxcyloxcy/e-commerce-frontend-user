59aa1ceeec1bc56f03ff92def1476528da07b4b9
some3L <luckyllemos0909@gmail.com>
2025-07-22 22:45:01 +0800

    Update base URL for main branch

--- src/assets/baseURL/api.js
+++ src/assets/baseURL/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://ec2-52-194-187-214.ap-northeast-1.compute.amazonaws.com/',
-  // baseURL: 'https://api.therelovedmarketplace.com/',
+  // baseURL: 'http://ec2-52-194-187-214.ap-northeast-1.compute.amazonaws.com/',
+  baseURL: 'https://api.therelovedmarketplace.com/',
   headers: { 'token': 'cCW7PW2CRotxuALrBuMob5lXgVhY4xo' }
 });
