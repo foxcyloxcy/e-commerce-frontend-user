ab3e5b1c3745ed7514f8a94d54fd37a28c19c57c
some3L <luckyllemos0909@gmail.com>
2025-08-06 20:48:05 +0800

    Update add HomeListItem component

--- src/components/HomeComponent/Home.jsx
+++ src/components/HomeComponent/Home.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Grid } from '@mui/material';
+import { Grid, Box, Typography, Button } from '@mui/material';
 import FeaturedProducts from '../FeaturedProductsComponent/FeaturedProducts';
 import FeaturedHero from '../HeroComponent/FeaturedHero';
 import Swal from 'sweetalert2';
 import ModTheme from '../ThemeComponent/ModTheme';
+import HomeListItem from '../HomeListItem/HomeListItem';
 
 
 export default function Home(props) {
@@ -12,23 +13,22 @@ export default function Home(props) {
     const history = useNavigate();
 
     const bankDetailsAlert = async (parsedUserData) => {
-        if (parsedUserData.has_bank_details === "No" || parsedUserData.vendor_bank === null) 
-                {
-                Swal.fire({
-                    title: 'Read me',
-                    text: "Please visit your My Profile page and create your bank details. You are required to add bank details before you can post an item.",
-                    icon: 'warning',
-                    confirmButtonText: 'Go to My Profile',
-                    confirmButtonColor: ModTheme.palette.primary.main,
-                    showCancelButton: true,
-                    cancelButtonText: 'I will do it later'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        history("/my-profile");
-                        refreshParent()
-                    }
-                })
-            }
+        if (parsedUserData.has_bank_details === "No" || parsedUserData.vendor_bank === null) {
+            Swal.fire({
+                title: 'Read me',
+                text: "Please visit your My Profile page and create your bank details. You are required to add bank details before you can post an item.",
+                icon: 'warning',
+                confirmButtonText: 'Go to My Profile',
+                confirmButtonColor: ModTheme.palette.primary.main,
+                showCancelButton: true,
+                cancelButtonText: 'I will do it later'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    history("/my-profile");
+                    refreshParent()
+                }
+            })
+        }
     }
 
     useEffect(() => {
@@ -46,7 +46,10 @@ export default function Home(props) {
                 <FeaturedProducts />
             </Grid>
 
-
+            {/* List an Item */}
+            <Grid item xs={12}>
+                <HomeListItem />
+            </Grid>
         </Grid>
     )
 }
