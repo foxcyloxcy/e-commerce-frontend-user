9afc959a260dc516d608adbe2d6d551b7dc32bb4
some3L <luckyllemos0909@gmail.com>
2025-08-06 21:15:19 +0800

    Update adjust margintop and marginbotton

--- src/components/FeaturedProductsComponent/FeaturedProducts.jsx
+++ src/components/FeaturedProductsComponent/FeaturedProducts.jsx
@@ -156,7 +156,7 @@ const FeaturedProducts = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 4,
                     slidesToScroll: 1,
                     infinite: true,
                     dots: false,
@@ -205,9 +205,9 @@ const FeaturedProducts = () => {
         <ThemeProvider theme={ModTheme}>
             <Box
                 sx={{
-                    padding: ModTheme.spacing(3),
-                    marginTop: 2,
-                    marginBottom: 10,
+                    px: ModTheme.spacing(4),
+                    marginTop: 1,
+                    marginBottom: 2,
                     WebkitOverflowScrolling: 'touch'
                 }}
             >
