7bf559df287201ff38593b56830d523ba0e5349b
some3L <luckyllemos0909@gmail.com>
2025-08-14 10:48:41 +0800

    Update add on-your-behalf-terms route

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,8 @@ import MyProductDetails from './components/UserProfileComponent/MyProductsCompon
 import FeatureItemSuccess from './components/PaymentSuccessComponent/FeatureItemSuccess';
 import VisitorPaymentSuccess from './components/PaymentSuccessComponent/VisitorPaymentSuccess'
 import CookiesPolicy from './staticPages/CookiePolicyComponent/CookiesPolicy';
-import OnYourBehalf from './components/OnYourBehalfComponent/Conceirge';
+import OnYourBehalf from './components/OnYourBehalfComponent/Concierge';
+import OnYourBehalfTerms from './staticPages/OnYourBehalfTermsComponent/ConciergeTerms';
 import useHotjar from './hooks/useHotJar'
 
 function App() {
@@ -155,6 +156,7 @@ function App() {
         <Route path="/privacy-policy" element={<PrivacyPolicy parentIsLoggedIn={isLoggedIn} />} />
         <Route path="/cookies-policy" element={<CookiesPolicy parentIsLoggedIn={isLoggedIn} />} />
         <Route path="/buyer-and-seller-terms" element={<BuyerAndSellerTerms parentIsLoggedIn={isLoggedIn} />} />
+        <Route path="/on-your-behalf-terms" element={<OnYourBehalfTerms parentIsLoggedIn={isLoggedIn} />} />
 
         <Route
           path="/add-product"
