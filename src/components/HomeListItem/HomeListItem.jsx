9f5d68be8198a1fa4f9db39880010299940ec5ed
some3L <luckyllemos0909@gmail.com>
2025-09-02 20:55:15 +0800

    Update remove how it works button

--- src/components/HomeListItem/HomeListItem.jsx
+++ src/components/HomeListItem/HomeListItem.jsx
@@ -8,17 +8,17 @@ const HomeListItem = () => {
     return (
         <ThemeProvider theme={ModTheme}>
             <Box sx={{ textAlign: 'center', py: 2, bgcolor: '#f7f7f7' }}>
-                <a href="/how-it-works"
+                {/* <a href="/how-it-works"
                     rel="noopener noreferrer">
                     <Button variant="contained" sx={{ bgcolor: ModTheme.palette.primary.main, mr: 2 }}>
                         How it works
                     </Button>
-                </a>
+                </a> */}
 
                 <a href="/shop?page=1&sort=1&category_id=&category_name=&sub_category_id=&sub_category_name=&filter_min_price=&filter_max_price=&filter_keyword=&filter_properties="
                     rel="noopener noreferrer">
-                    <Button variant="contained" sx={{ bgcolor: ModTheme.palette.primary.main, ml: 2 }}>
-                        Browse items
+                    <Button variant="contained" sx={{ bgcolor: ModTheme.palette.primary.main }}>
+                        Shop Reloved
                     </Button>
                 </a>
             </Box>
