64714b91586cbda06635e44bb866a1c4ded6a3b2
some3L <luckyllemos0909@gmail.com>
2026-02-24 21:16:59 +0800

    Add newhomepage component

---
+++ src/components/NewHomePageComponent/NewHomePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import HeroSection from "./sections/HeroSection";
+import AboutSection from "./sections/AboutSection";
+import BenefitsSection from "./sections/BenefitsSection";
+import JoinSection from "./sections/JoinSection";
+import ModTheme from "../ThemeComponent/ModTheme";
+import { ThemeProvider } from "@mui/material";
+
+
+export default function NewHomepage() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <BenefitsSection />
+      <JoinSection />
+    </>
+  );
+}
