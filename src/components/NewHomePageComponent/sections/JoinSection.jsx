80e2cd0b556e925d6d037b8dd63ff26b95786399
some3L <luckyllemos0909@gmail.com>
2026-02-24 21:18:02 +0800

    Add join section for new home page

---
+++ src/components/NewHomePageComponent/sections/JoinSection.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Container, Typography, Button } from "@mui/material";
+
+export default function JoinSection() {
+  return (
+    <Box sx={{ py: 10, textAlign: "center" }}>
+
+      <Container maxWidth="md">
+
+        <Typography variant="h4" gutterBottom>
+
+          How to Join
+
+        </Typography>
+
+        <Typography sx={{ mb: 1 }}>
+          1. Register your interest
+        </Typography>
+
+        <Typography sx={{ mb: 1 }}>
+          2. Join WhatsApp group
+        </Typography>
+
+        <Typography sx={{ mb: 3 }}>
+          3. Start discussion
+        </Typography>
+
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#25d366"
+          }}
+        >
+          Join Discussion
+        </Button>
+
+      </Container>
+
+    </Box>
+  );
+}
