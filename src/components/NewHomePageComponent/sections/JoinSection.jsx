d337088b116233c146d253effa61c5c7177398fd
some3L <luckyllemos0909@gmail.com>
2026-02-28 09:09:28 +0800

    Update add modal

--- src/components/NewHomePageComponent/sections/JoinSection.jsx
+++ src/components/NewHomePageComponent/sections/JoinSection.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Container, Typography, Button } from "@mui/material";
+import ModTheme from "../../ThemeComponent/ModTheme";
+import NewUserLeadsModal from "../../ReusableComponents/ModalComponent/NewUserLeadsModal"
 
 export default function JoinSection() {
+
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <Box sx={{ py: 10, textAlign: "center" }}>
+    <Box sx={{ py: 10, textAlign: "center", backgroundColor: ModTheme.palette.secondary.dark }}>
 
       <Container maxWidth="md">
 
         <Typography variant="h4" gutterBottom>
-
           How to Join
-
         </Typography>
 
         <Typography sx={{ mb: 1 }}>
@@ -28,13 +31,20 @@ export default function JoinSection() {
         <Button
           variant="contained"
           sx={{
-            backgroundColor: "#25d366"
+            backgroundColor: ModTheme.palette.primary.main
           }}
+          onClick={() => setOpenModal(true)}
         >
           Join waitlist
         </Button>
 
       </Container>
+
+
+      <NewUserLeadsModal
+        open={openModal}
+        handleClose={() => setOpenModal(false)}
+      />
 
     </Box>
   );
