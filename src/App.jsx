5b84a15fe6715f74a3c5a1905b58e430f4e599bd
some3L <luckyllemos0909@gmail.com>
2026-03-04 12:21:46 +0800

    Update add hotjar import incase we putting it back

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ import VisitorPaymentSuccess from './components/PaymentSuccessComponent/VisitorP
 import CookiesPolicy from './staticPages/CookiePolicyComponent/CookiesPolicy';
 import OnYourBehalf from './components/OnYourBehalfComponent/Concierge';
 import OnYourBehalfTerms from './staticPages/OnYourBehalfTermsComponent/ConciergeTerms';
+// import useHotjar from './hooks/useHotJar'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState("");
